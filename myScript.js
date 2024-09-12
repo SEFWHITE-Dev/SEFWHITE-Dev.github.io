0bcab4579015b2991068829cbc83aef6a9605dc5
WhiteNoise01 <153906600+SEFWHITE-Dev@users.noreply.github.com>
2024-09-12 11:32:07 +0900

    changes

--- myScript.js
+++ myScript.js
@@ -1,5 +1,5 @@
 $('[lang]').hide(); // hide all lang attributes on start.
-$('[lang="en"]').show(); // show just Japanese text (you can change it)
+$('[lang="en"]').show(); // show just English text 
 $('#lang-switch').change(function () { // put onchange event when user select option from select
     var lang = $(this).val(); // decide which language to display using switch case. The rest is obvious (i think)
     switch (lang) {
